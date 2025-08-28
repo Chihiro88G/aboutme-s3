ba17fb5b824b2b1583cef8b2de97445718b1845b
Chihiro88G <1114chihiro@gmail.com>
2025-08-28 14:47:46 -0400

    temporary added a modal while resume is being updated

--- src/components/Home/ResumeBtn.js
+++ src/components/Home/ResumeBtn.js
@@ -1,20 +1,52 @@
-import { Button, useMediaQuery } from '@mui/material';
+import { useState } from 'react';
+import { Button, useMediaQuery, Modal, Box, Typography } from '@mui/material';
 import PictureAsPdfIcon from '@mui/icons-material/PictureAsPdf';
 
 export default function ResumeBtn() {
   const isDesktop = useMediaQuery('(min-width: 960px)');
 
+  const [open, setOpen] = useState(false);
+  const handleOpen = () => setOpen(true);
+  const handleClose = () => setOpen(false);
+
+  const modalStyle = {
+    position: 'absolute',
+    top: '50%',
+    left: '50%',
+    transform: 'translate(-50%, -50%)',
+    width: 400,
+    bgcolor: 'background.paper',
+    boxShadow: 24,
+    p: 4,
+  };
+
   return (
-    <Button
-      variant='contained'
-      sx={{
-        mt: isDesktop ? '60px' : '20px',
-      }}
-      color='success'
-      download='ChihiroH_Resume.pdf'
-      href='ChihiroH_Resume.pdf'
-    >
-      <PictureAsPdfIcon />RESUME
-    </Button>
+    <>
+      <Button
+        variant='contained'
+        sx={{
+          mt: isDesktop ? '60px' : '20px',
+        }}
+        color='success'
+        // download='ChihiroH_Resume.pdf'
+        // href='ChihiroH_Resume.pdf'
+        onClick={handleOpen}
+      >
+        <PictureAsPdfIcon />RESUME
+      </Button>
+
+      <Modal
+        open={open}
+        onClose={handleClose}
+        aria-labelledby="modal-modal-title"
+        aria-describedby="modal-modal-description"
+      >
+        <Box sx={modalStyle}>
+          <Typography id="modal-modal-title" variant="h6" component="h2">
+            Update resume coming soon...!
+          </Typography>
+        </Box>
+      </Modal>
+    </>
   );
 }
